0b1f59e00b99a5e5767573e5d03f5d181ea4eb71
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-07-09 21:10:33 -0400

    config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { TodosModule } from './todos/todos.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     TodosModule,
     AuthModule,
     MongooseModule.forRoot(process.env.MONGODB),
